0a631679c6e1dd77fb5d22e1a926191e4250f202
JulioCAC <juliocac999@gmail.com>
2024-09-03 16:00:57 -0300

    questao 4 resolvida

--- desafioTecnico/src/Main.java
+++ desafioTecnico/src/Main.java
@@ -11,6 +11,13 @@ public class Main {
         System.out.println("Questão 3");
         Soma soma = new Soma();
         soma.calculateSoma();
+        System.out.println("Questão 4");
+        System.out.println("a) Próximo elemento: 9 (numeros impares)");
+        System.out.println("b) Próximo elemento: 128 (potências de 2)");
+        System.out.println("c) Próximo elemento: 49 (quadrados perfeitos)");
+        System.out.println("d) Próximo elemento: 100, (quadrados dos numeros pares) ");
+        System.out.println("e) Próximo elemento: 13 (fibonacci)");
+        System.out.println("f) Próximo elemento: 200 (numeros que começam com D)");
 
 
     }
